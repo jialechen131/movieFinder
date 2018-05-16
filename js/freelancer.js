251b867210335148f0a3ba37c09cae86df9beacd
Louis Gabrie <louisgabrie1450@gmail.com>
2018-05-16 20:27:36 +0000

    I fixed up the aesthics of the home page, I fixed the modals, and I fixed up the css.

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,3 +1,67 @@
+/* global $ */
+$(document).ready(function() {
+    console.log("document ready")
+    $("#srch-term").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#submit").click();
+        }
+    });
+
+    $("#submit").click(function() {
+        console.log("clicked");
+        $(".gallery").empty();
+        var userInput = $('#srch-term').val();
+        var url = "https://omdbapi.com?apikey=90d4b10a&s=" + userInput.toLowerCase();
+        $.ajax({
+            url: url,
+            method: "GET",
+            success: function(response) {
+                $("#moviesSearchTerm").html(userInput);
+                console.log(response);
+                if (response.Response == "False") {
+                    console.log("I work");
+                    $('.gallery').append(
+                        '<p class="warningSign">' + "No movie was found with those characters..." + '</p>'
+                    );
+                }
+                console.log("I work 2");
+                for (var i = 0; i < response.Search.length; i++) {
+                  console.log(response.Search[i].title);
+                    if (response.Search[i].Poster !== "N/A") {
+                        $('.gallery').append(
+                            '<img data-toggle="modal" data-target="#modal-' + i + '" class="col-md-3 movies" src=' + response.Search[i].Poster + '>\
+			                  <div class="modal" id="modal-' + i + '" tabindex="-1" aria-labelledby="myModalLabel">\
+			                     <div class="modal-dialog">\
+			                         <div class="modal-content">\
+			                             <div class="modal-header">\
+			                                 <h4 class="modal-title" id="myModalLabel">' + response.Search[i].Title + '</h4>\
+			                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
+			                             </div>\
+			                             <div class="modal-body">\
+			                                 <img class="modalImage" src="' + response.Search[i].Poster + '">\
+			                                 <div class="gif_description">\
+			                                     <p>Uploaded: Hello </p>\
+			                                      <p>Rating: ' + response.Search[i].Title + '</p>\
+			                                     <p>GIF Link: ' + response.Search[i].Title + '</p>\
+			                                 </div>\
+			                             </div>\
+			                         </div>\
+			                     </div>\
+			                  </div>'
+                        );
+                        console.log("The amount of Movies being displayed to the screen: " + response.Search.length);
+                        console.log("The total amount of possible Movies " + response.Search[1].totalResults);
+                    }
+                }
+            },
+        });
+    });
+});
+
+
+
+
+
 (function($) {
   "use strict"; // Start of use strict
 
@@ -73,3 +137,7 @@
   });
 
 })(jQuery); // End of use strict
+
+
+// Use this to access the information for the modal 
+// "https://api.themoviedb.org/3/movie/" + response.Search[i].imdbID + "?api_key=353765fef7f00a0fd099296ded0816a7"
